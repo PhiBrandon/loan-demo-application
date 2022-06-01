920bc70a1b6218e14955152cb36f755117dc937c
Brandon Phillips <thedefinedone@gmail.com>
2022-06-01 08:05:19 -0600

    revised how the variables are loaded at build time

--- pages/dashboardContentLayout.tsx
+++ pages/dashboardContentLayout.tsx
@@ -59,7 +59,7 @@ export default function DashboardContentLayout({ sessionData }: any) {
                       alt=""
                     />
                     <h1 className="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
-                      Good morning, {sessionData.user.name}
+                      Good morning, {sessionData?.user.name ? sessionData?.user.name : ("Molly")}
                     </h1>
                   </div>
                   <dl className="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
