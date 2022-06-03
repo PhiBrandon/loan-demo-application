8a2e370706340331080a981762918860292ff7af
Brandon Phillips <thedefinedone@gmail.com>
2022-06-03 08:51:04 -0600

    more script updates

--- scripts/start_pm2.sh
+++ scripts/start_pm2.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 source /home/ubuntu/.profile
 cd /home/ubuntu/loan-application
+echo ""
+echo ""
+echo ""
+env
+echo ""
+echo ""
+echo ""
 npm i -g pm2
 pm2 start npm --name "next" -- start -f
